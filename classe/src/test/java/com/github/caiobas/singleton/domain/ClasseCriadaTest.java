802310be3eecec286566a6d8462712ec9813183a
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2019-09-24 10:13:34 -0300

    Estratégia para tornar teste "forte" (não dependendo de considerações).

--- classe/src/test/java/com/github/caiobas/singleton/domain/ClasseCriadaTest.java
+++ classe/src/test/java/com/github/caiobas/singleton/domain/ClasseCriadaTest.java
@@ -8,6 +8,8 @@ import com.github.caiobas.autoclosable.domain.ClasseCriada;
 
 public class ClasseCriadaTest {
 
+	// FIXME o teste passa, mas a exceção capturada não é aquela gerada pelo throw no método close
+	// TODO trocar o null por um nome válido, existente. Uma estratégia adequada foi implementada pelo josenilton em https://github.com/newtonjose/ds-2019-02/blob/master/auto-closeable/src/test/java/com/github/newtonjose/autocloseable/FileInputStreamAutoCloseableTest.java
 	@Test
 	void verificaCloseChamado() {
 	    assertThrows(RuntimeException.class, () -> {
