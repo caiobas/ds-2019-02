4df1a6adc73fd62195e4baa2e6ce88ed07374f75
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2019-10-01 09:56:14 -0300

    Simplificacao (evita trazer para o seu código a aritmética)

--- comparable/src/main/java/com/github/caiobas/comparable/domain/ComparatorAluno.java
+++ comparable/src/main/java/com/github/caiobas/comparable/domain/ComparatorAluno.java
@@ -6,6 +6,8 @@ public class ComparatorAluno implements Comparator<Aluno>{
 
 	@Override
 	public int compare(Aluno aluno1, Aluno aluno2) {
+		// FIXME alternativa
+		// return Integer.compare(aluno1.getNome().length(), aluno2.getNome().length());
 		return aluno1.getNome().length() - aluno2.getNome().length();
 	}
 
